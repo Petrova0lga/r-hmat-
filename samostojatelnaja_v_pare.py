cfe52d75f3ea08597beab8646f9ca32e5117d2a5
opilane <opilane@HP-CZC6297B9B>
2022-12-13 09:52:29 +0200

    Add project files.

---
+++ samostojatelnaja_v_pare.py
@@ -0,0 +1,9 @@
+from math import * 
+from random import * 
+print("Isikukoodi kontrolimine")
+text=input("Palun kirjuta teieisikukoodi: ")
+n=len("text")
+if n==11 and text.isdigit():
+    print("hästi")
+else:
+    print("Sa kirjutasid palun või vähe sümboleid")
